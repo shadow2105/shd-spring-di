0a6b24732249b522b28c03879acf46510b8b6706
shadow2105 <ashwinrajput66@gmail.com>
2023-02-01 10:30:36 -0500

    Initial Project Add

---
+++ src/main/java/com/example/shdspringdi/ShdSpringDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.shdspringdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShdSpringDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShdSpringDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/shdspringdi/ShdSpringDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shdspringdi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShdSpringDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
