0e52342ae5f5688ffdd3cbc2baf8b3e647c9362c
shadow2105 <ashwinrajput66@gmail.com>
2023-02-04 12:03:09 -0500

    Add Example of Spring ApplicationContext - Inversion of Control -

--- src/main/java/com/example/shdspringdi/ShdSpringDiApplication.java
+++ src/main/java/com/example/shdspringdi/ShdSpringDiApplication.java
@@ -1,13 +1,42 @@
 package com.example.shdspringdi;
 
+import com.example.shdspringdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ShdSpringDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShdSpringDiApplication.class, args);
+
+		//public static ConfigurableApplicationContext run(Class<?> primarySource, String... args)
+		//run method returns an ApplicationContext
+		//SpringApplication.run(ShdSpringDiApplication.class, args);
+
+		//storing ApplicationContext returned by run
+		//https://www.baeldung.com/spring-application-context
+		ApplicationContext aCtx = SpringApplication.run(ShdSpringDiApplication.class, args);
+
+		/* https://www.baeldung.com/inversion-control-and-dependency-injection-in-spring
+		Instead of having to call the dependency class or library and create its object manually
+		to be used in the dependent class in our code, Frameworks utilizing Inversion of Control (a software engineering principle)
+		are responsible for creating, providing and managing the objects of dependency classes or libraries to the dependent class.
+
+		Dependency injection (a software design pattern based on IoC) utilizes the control being inverted to set
+		a dependent class object's dependencies (objects of other classes or libraries) at runtime (provided by Framework,
+		like Spring ApplicationContext) rather than creating and providing them manually while writing our code.
+		 */
+		//asking returned ApplicationContext for an instance of the controller
+		//no need to create the object manually as it's construction/instantiation is managed by the Spring ApplicationContext
+		MyController myController = (MyController) aCtx.getBean("myController");
+		//getBean() returns an object of type java.lang.Object and hence needs to be type-casted to an object controller class
+		//getBean() takes a string argument to specify the controller object name (stored as controller class name in camelcase)
+
+
+		//calling controller class object (returned by ApplicationContext) method(s)
+		String greeting = myController.sayHello();
+		System.out.println(greeting);
 	}
 
 }
---
+++ src/main/java/com/example/shdspringdi/controllers/MyController.java
@@ -0,0 +1,13 @@
+package com.example.shdspringdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("Hello World!!");
+
+        return "Hi Folks!";
+    }
+}
