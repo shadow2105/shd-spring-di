9ac04e3f136753827b88b2ddb7d2c33e2b22cec0
shadow2105 <ashwinrajput66@gmail.com>
2023-03-15 22:12:36 -0400

    Add Demo of Spring-boot application.properties

--- src/main/java/com/example/shdspringdi/config/ServiceConfig.java
+++ src/main/java/com/example/shdspringdi/config/ServiceConfig.java
@@ -11,7 +11,7 @@ import com.example.shdspringdi.services.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
-@PropertySource("classpath:datasource.properties")
+
 @ImportResource("classpath:service-config.xml")     //or annotate the Application class (ShdSpringDiApplication)
 @Configuration
 public class ServiceConfig {
